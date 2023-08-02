781f426a7903429cdd65f2eb3a2d8d37958cd597
srizonsansar <srizonsansar@gmail.com>
2023-08-02 14:59:17 +0100

    Mobile Nav done with use-state

--- src/pages/common/Header.js
+++ src/pages/common/Header.js
@@ -1,6 +1,9 @@
 import React, { useState }from 'react';
-import Navigation from '../common/Navigation';
+import { NavLink } from "react-router-dom";
+
+// Assets
 import MainLogo from '../../assets/images/logo-cogedim.svg';
+import IconCross from '../svg-icons/Icon-Cross';
 
 const Header = () => {
 
@@ -15,18 +18,36 @@ const Header = () => {
         }
     })
 
-    // On Click show / hide Nav
-    const bodyTag = document.querySelector('body')
-    const showNavBlock = () => {
-        bodyTag.classList.add('no-scroll')
+    const [isActive, setIsActive] = useState(false);
+    const handleShowNav = () => {
+        setIsActive(!isActive)
+    }
+    const handleHideNav = () => {
+        setIsActive(!isActive)
     }
 
     return(
         <header className="header">
             <div className="header__container">
-                <a href="/" className="header__logo"><img src={MainLogo} className="header__logo__img" alt="Cogedim" /></a>
-                <Navigation />
-                <div className="header__navLines" onClick={showNavBlock}>
+                <div className="header__logo">
+                    <NavLink to="/"><img src={MainLogo} className="header__logo__img" alt="Cogedim" /></NavLink>
+                </div>
+                <div className={`header__mainBlock ${isActive ? 'visible' : ''}`}>
+                    <div className="header__mainBlock__closeNav" onClick={handleHideNav}>
+                        <IconCross/>
+                    </div>
+                    <div className="header__nav">
+                        <NavLink to="/">Le Quartier</NavLink>
+                        <NavLink to="/">Les Résidences</NavLink>
+                        <NavLink to="/">Les Actualités</NavLink>
+                    </div>
+                    <div className="header__social">
+                        <a href="https://www.facebook.com/" rel="noreferrer" className="header__social__item" target="_blank">F</a>
+                        <a href="https://www.facebook.com/" rel="noreferrer" className="header__social__item" target="_blank">T</a>
+                        <a href="https://www.facebook.com/" rel="noreferrer" className="header__social__item" target="_blank">I</a>
+                    </div>
+                </div>
+                <div className="header__navLines" onClick={handleShowNav}>
                     <span className="header__navLines__line"></span>
                     <span className="header__navLines__line header__navLines__line--mid"></span>
                     <span className="header__navLines__line"></span>
